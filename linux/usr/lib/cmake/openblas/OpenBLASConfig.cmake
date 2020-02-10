9da2143ad0e019e869d3fc9ae4cc0f49e5bf04fa
Clement DAVID <clement.david@esi-group.com>
2020-02-10 16:53:36 +0100

    linux: rebuild with gcc 8.3

--- linux/usr/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.20")
-SET(OpenBLAS_INCLUDE_DIRS /home/scilab/work/scilab-prerequirements/linux/usr/include)
-SET(OpenBLAS_LIBRARIES /home/scilab/work/scilab-prerequirements/linux/usr/lib/libopenblas.so)
--- linux/usr/lib/xml2Conf.sh
+++ linux/usr/lib/xml2Conf.sh
@@ -2,7 +2,7 @@
 # Configuration file for using the XML library in GNOME applications
 #
 XML2_LIBDIR="-L/lib"
-XML2_LIBS="-lxml2 -L/home/scilab/work/linux-prerequisites-sources/trunk/Dev-Tools/SE/Prerequirements/linux/usr/lib -lz   -lm "
+XML2_LIBS="-lxml2 -L/home/buildscilab/work/scilab-prerequirements/linux/usr/lib -lz     -lm "
 XML2_INCLUDEDIR="-I/include/libxml2"
-MODULE_VERSION="xml2-2.9.1"
+MODULE_VERSION="xml2-2.9.9"
 
