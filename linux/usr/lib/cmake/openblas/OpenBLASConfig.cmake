7ac5817bfef1225a2b09410c679c7a80ac07debb
Clement DAVID <clement.david@esi-group.com>
2021-07-05 15:19:40 +0200

    linux: refresh with latest changes from linux_x64
     * update JRE to 1.8.0 292
     * remove libldap dependency on libcurl

---
+++ linux/usr/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.20")
+SET(OpenBLAS_INCLUDE_DIRS /home/scilab/work/scilab-prerequirements/linux/usr/include)
+SET(OpenBLAS_LIBRARIES /home/scilab/work/scilab-prerequirements/linux/usr/lib/libopenblas.so)
